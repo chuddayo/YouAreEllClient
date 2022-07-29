8168edb1a60853e148c11b3aaadfca7c1e2acbaf
Ryan Chudd <ryan.a.chudd@gmail.com>
2022-07-28 21:52:46 -0400

    server controller refactor

--- Client/src/main/java/controllers/ServerController.java
+++ Client/src/main/java/controllers/ServerController.java
@@ -1,10 +1,7 @@
 package controllers;
 
-import kong.unirest.HttpResponse;
-import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
 import java.net.MalformedURLException;
-import java.net.URL;
 
 public class ServerController {
     private final String rootURL = "http://zipcode.rocks:8085";
@@ -19,27 +16,22 @@ public class ServerController {
         send server GET request and return body as JSON string
     */
     public String getURL(String urlExtension) throws MalformedURLException {
-        URL url = new URL(rootURL + "/" + urlExtension);
-        HttpResponse<JsonNode> jsonResponse = Unirest.get(url.toString()).asJson();
-        return jsonResponse.getBody().toString();
+        return Unirest.get(rootURL + "/" + urlExtension).asJson().getBody().toString();
     }
 
     /*
         send server POST request and return a JSON string result
     */
-    public String postURL(String urlExtension, String jsonBody) throws MalformedURLException {
-        URL url = new URL(rootURL + "/" + urlExtension);
-        HttpResponse<JsonNode> jsonResponse = Unirest.post(url.toString()).body(jsonBody).asJson();
-        return jsonResponse.getBody().toString();
+    public String postURL(String urlExtension, String jsonBody) {
+        return Unirest.post(rootURL + "/" + urlExtension).body(jsonBody).asJson().getBody().toString();
     }
 
     /*
         send server PUT request and return a JSON string result
     */
-    public String putURL(String urlExtension, String jsonBody) throws MalformedURLException {
-        URL url = new URL(rootURL + "/" + urlExtension);
-        HttpResponse<JsonNode> jsonResponse = Unirest.put(url.toString()).body(jsonBody).asJson();
-        //System.out.println(jsonResponse.getBody().toString());
-        return jsonResponse.getBody().toString();
+    public String putURL(String urlExtension, String jsonBody) {
+        return Unirest.put(rootURL + "/" + urlExtension).body(jsonBody).asJson().getBody().toString();
     }
+
+    //HttpResponse<JsonNode> jsonResponse = Unirest.get(rootURL + "/" + urlExtension).asJson();
 }
--- Client/src/main/java/controllers/TransactionController.java
+++ Client/src/main/java/controllers/TransactionController.java
@@ -1,11 +1,6 @@
 package controllers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import models.Id;
-import models.Message;
-
-import java.net.MalformedURLException;
 
 public class TransactionController {
     private MessageController msgCtrl;
@@ -18,53 +13,11 @@ public class TransactionController {
         idCtrl = j;
     }
 
-
-//    public String makeRequest(String urlExtension, String requestType, String payload) throws JsonProcessingException, MalformedURLException {
-//        //
-//        if (!payload.equals("") && !requestType.equals("GET")) {
-//            if (urlExtension.equals("ids")) {
-//                Id payloadId = mapper.readValue(payload, Id.class);
-//                // TODO POST OR PUT
-//                return getIdCtrl().postId(payloadId);
-//            } else if (urlExtension.equals("messages")) {
-//                Message payloadMessage = mapper.readValue(payload, Message.class);
-//                // TODO POST OR PUT
-//                return getMsgCtrl().postMessage(payloadMessage);
-//            }
-//        }
-//
-//        if (urlExtension.equals("ids")) { // request idController
-//            if (requestType.equals("GET")) return getIdCtrl().getIdsAsList();
-//        } else { // request messageController
-//
-//        }
-//        return null;
-//    }
-
-//    public List<Id> getIds() {
-//        return null;
-////        return idCtrl.getIds();
-//    }
-
-//    public String postId(String idToRegister, String githubName) {
-////        Id tid = new Id(idToRegister, githubName);
-////        tid = idCtrl.postId(tid);
-//        return ("Id registered.");
-//    }
-
     public MessageController getMsgCtrl() {
         return msgCtrl;
     }
 
-    public void setMsgCtrl(MessageController msgCtrl) {
-        this.msgCtrl = msgCtrl;
-    }
-
     public IdController getIdCtrl() {
         return idCtrl;
-    }
-
-    public void setIdCtrl(IdController idCtrl) {
-        this.idCtrl = idCtrl;
     }
 }
